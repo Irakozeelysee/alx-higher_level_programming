df6cfebfb4597f4522cfb89a08e43d34f5d51c2c
Irakozeelysee <elyseeirakoze@gmail.com>
2023-07-04 15:11:03 +0000

    At every point of life the eyes see less than the mind

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,34 @@
+def matrix_mul(m_a, m_b):
+    """Multiplies two matrices."""
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
+        raise TypeError("m_a must be a list or m_b must be a list")
+
+    if not (all(isinstance(row, list) for row in m_a)
+            or not all(isinstance(row, list) for row in m_b)):
+        raise TypeError("m_a must be a list of lists or "
+                        "m_b must be a list of lists")
+
+    if (m_a == [] or m_b == [] or any(row == [] for row in m_a)
+            or any(row == [] for row in m_b)):
+        raise ValueError("m_a can't be empty or m_b can't be empty")
+
+    if (any(not all(isinstance(num, (int, float))
+                    for num in row) for row in m_a)):
+        raise TypeError("m_a should contain only integers or floats")
+
+    if (any(not all(isinstance(num, (int, float))
+                    for num in row) for row in m_b)):
+        raise TypeError("m_b should contain only integers or floats")
+
+    if (any(len(row) != len(m_a[0]) for row in m_a)
+            or any(len(row) != len(m_b[0]) for row in m_b)):
+        raise TypeError("each row of m_a must be of the same size"
+                        "or each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    # Perform matrix multiplication
+    result = [[sum(a * b for a, b in zip(row_a, col_b))
+               for col_b in zip(*m_b)] for row_a in m_a]
+    return result
