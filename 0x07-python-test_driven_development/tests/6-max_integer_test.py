6fc94c91b46957a8cdd7a39f7efdda94f0807474
Irakozeelysee <elyseeirakoze@gmail.com>
2023-07-06 14:29:06 +0000

    Let keep challenging ourselves with doing even hard things

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,11 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+"""
+unittest for max_integer function
+"""
+
 import unittest
 
 max_integer = __import__("6-max_integer").max_integer
-
-"""unittest for max_integer function
-"""
 
 
 class TestMaxInteger(unittest.TestCase):
@@ -12,11 +13,25 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_ModuleDoc(self):
         """check the module's doc style"""
-        self.assertTrue(__import__("6-max_integer").__doc__)
+        doc = __import__("6-max_integer").__doc__
+        self.assertTrue(len(doc) > 1)
 
     def test_FunctionDoc(self):
         """check the function's doc style"""
-        self.assertTrue(__import__("6-max_integer").max_integer.__doc__)
+        doc = max_integer.__doc__
+        self.assertTrue(len(doc) > 1)
+
+    def test_MaxAtBeginning(self):
+        """max at the beginning of the list case"""
+        self.assertEqual(100, max_integer([100, 5, 4]))
+
+    def test_MaxAtMid(self):
+        """max at the middle of the list case"""
+        self.assertEqual(45, max_integer([10, 5, 45, 44, 4]))
+
+    def test_MaxAtEnd(self):
+        """max at the end of the list case"""
+        self.assertEqual(544, max_integer([10, 5, 45, 44, 4, 544]))
 
     def test_emptyList(self):
         """empty list case"""
@@ -36,7 +51,8 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_listMixedNoInt(self):
         """list elements not all int"""
-        self.assertRaises(TypeError, max_integer([13, "h", 44, "C"]))
+        with self.assertRaises(TypeError):
+            max_integer([13, "h", 44, "C"])
 
 
 if __name__ == "__main__":
