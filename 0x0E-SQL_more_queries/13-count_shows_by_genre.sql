1f2fa8f0c9d40bf29529431d531056a349323df8
Irakozeelysee <elyseeirakoze@gmail.com>
2023-08-18 11:55:21 +0000

    Script to list all shows with linked genres

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Script to list all genres and their linked show counts
+
+SELECT tv_genres.genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.genre
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
