c7a29f81593f45d760dc6053a9079b27878e0f81
Irakozeelysee <elyseeirakoze@gmail.com>
2023-09-16 16:08:01 +0000

    better late than never

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const BaseSquare = require('./5-square');
+
+module.exports = class Square extends BaseSquare {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+};
