08dddb12004cf6cb37750e85699e956ce737d9c5
Irakozeelysee <elyseeirakoze@gmail.com>
2023-09-17 17:26:17 +0000

    My First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""
-Module to define the State class.
+"""Module to define the State class.
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+
 Base = declarative_base()
 
 
-class City(Base):
+class State(Base):
     """Class representing a state in the database.
     Attributes:
-    id (int): An auto-generated,
-    unique integer representing the primary key.
+    id (int): An auto-generated, unique integer representing the primary key.
     name (str): A string with a maximum of
     128 characters representing the state name.
     """
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = column(Integer, ForeignKey("states.id"), nullable=False)
