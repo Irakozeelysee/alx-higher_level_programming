0678a2ae8a81112e52a58a303a796d0f5b03323d
Irakozeelysee <elyseeirakoze@gmail.com>
2023-09-17 16:50:48 +0000

    Cities by states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-Filter states
+SQL Injection...
 """
-
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost',
+    db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id"
-    cur.execute(query)
+    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id"
 
+    cur.execute(query, (argv[4],))
     for row in cur.fetchall():
         print(row)
 
---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+List all cities from the database.
+
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name \
+            FROM cities
+            INNER JOIN states ON states.id = cities.state_id"
+
+    cur.execute(query)
+    for record in cur.fetchall():
+        print(record)
+
+    cur.close()
+    db.close()
